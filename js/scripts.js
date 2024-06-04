1a6925bd00d7916ccdcb78dcdf880aa22f4612be
may_ <lammayyin@gmail.com>
2024-06-04 17:02:25 +0200

    Minor debug
    Added commas where necessary

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {id: 1, name: 'Bulbasaur', type: ['grass','poison']}
-    {id: 2, name: 'Ivysaur', type: ['grass','poison']}
+    {id: 1, name: 'Bulbasaur', type: ['grass','poison'] },
+    {id: 2, name: 'Ivysaur', type: ['grass','poison']},
     {id: 3, name: 'Venusaur', type: ['grass','poison']}
 ];
