8278b5f6df015a5d13ce3c6ce3af4ed153f21e85
may_ <lammayyin@gmail.com>
2024-06-04 17:01:25 +0200

    Added pokemonList and 3 Pokemon data
    Removed test scripts, added 3 pokemon to pokemon List with id, name, and type objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World ');
-let favoriteFood='Hainanese Roasted Chicken Rice';
-document.write(favoriteFood);
+let pokemonList = [
+    {id: 1, name: 'Bulbasaur', type: ['grass','poison']}
+    {id: 2, name: 'Ivysaur', type: ['grass','poison']}
+    {id: 3, name: 'Venusaur', type: ['grass','poison']}
+];
