764405e01d0d70e4f238c0ebaadbb5d110f71ff8
may_ <lammayyin@gmail.com>
2024-06-05 16:13:41 +0200

    Added height and display in HTML
    Added height to pokemonList, displays pokemon name and height in .html file, exclaims 'Wow, that's Big' if the pokemon is above 6 in height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 let pokemonList = [
-    {id: 1, name: 'Bulbasaur', type: ['grass','poison'] },
-    {id: 2, name: 'Ivysaur', type: ['grass','poison']},
-    {id: 3, name: 'Venusaur', type: ['grass','poison']}
+    {
+        id: 1, 
+        name: 'Bulbasaur',
+        type: ['grass', 'poison'],
+        height: 2.04
+    },
+    { 
+        id: 2,
+        name: 'Ivysaur',
+        type: ['grass', 'poison'],
+        height: 3.03
+    },
+    { 
+        id: 3,
+        name: 'Venusaur',
+        type: ['grass', 'poison'],
+        height: 6.07 
+    }
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    document.write(pokemon.name + " (height: " + pokemon.height + ")");
+    if (pokemon.height > 6) {
+        document.write(" - Wow, that\'s Big!");
+    }
+    document.write("</br>");
+};
