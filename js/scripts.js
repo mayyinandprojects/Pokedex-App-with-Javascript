c9463476b4f0d1a30d9b86b4db666028983312a2
may_ <lammayyin@gmail.com>
2024-06-11 13:05:43 +0200

    Updated for loop
    Updated for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,19 @@ let pokemonList = [
         height: 6.07 
     }
 ];
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
-    document.write(pokemon.name + " (height: " + pokemon.height + ")");
-    if (pokemon.height > 6) {
-        document.write(" - Wow, that\'s Big!");
-    }
+// for (let i = 0; i < pokemonList.length; i++) {
+//     let pokemon = pokemonList[i];
+//     document.write(pokemon.name + " (height: " + pokemon.height + ")");
+//     if (pokemon.height > 6) {
+//         document.write(" - Wow, that\'s Big!");
+//     }
+//     document.write("</br>");
+// };
+
+pokemonList.forEach( pokemon => {
+    document.write(pokemon.name)
+        if (pokemon.height > 6) {
+                document.write(" - Wow, that\'s Big!");
+                }
     document.write("</br>");
-};
+} );
